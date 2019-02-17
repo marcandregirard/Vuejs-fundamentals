7fe30be8f838abad8c6daa4852c0501c82100d7d
Marc-Andre Girard <marcandr.girard@gmail.com>
2019-02-17 14:18:25 -0500

    Mixin example in the application

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -59,6 +59,7 @@
 
 <script>
 import availableParts from '../data/parts';
+import createdHookMixin from './created-hook-mixin';
 
 function getPreviousValidIndex(index, length) {
   const deprecatedIndex = index - 1;
@@ -72,9 +73,6 @@ function getNextValidIndex(index, length) {
 
 export default {
   name: 'RobotBuilder',
-  created() {
-    console.log('component created');
-  },
   data() {
     return {
       availableParts,
@@ -86,6 +84,7 @@ export default {
       selectedBaseIndex: 0,
     };
   },
+  mixins: [createdHookMixin],
   computed: {
     saleBorderClass() {
       return this.selectedRobot.head.onSale ? 'sale-border' : '';
