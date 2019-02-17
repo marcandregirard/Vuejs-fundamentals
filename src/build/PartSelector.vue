984a594e4198c1cdf8b2d1f940ac3858de89e65d
Marc-Andre Girard <marcandr.girard@gmail.com>
2019-02-17 15:53:51 -0500

    Added type and value validation

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -17,7 +17,19 @@ function getNextValidIndex(index, length) {
   return incrementedIndex > length - 1 ? 0 : incrementedIndex;
 }
 export default {
-  props: ['parts', 'position'],
+  props: {
+    parts: {
+      type: Array,
+      required: true,
+    },
+    position: {
+      type: String,
+      required: true,
+      validator(value) {
+        return ['left', 'right', 'top', 'bottom', 'center'].includes(value);
+      },
+    },
+  },
   data() {
     return { selectedPartIndex: 0 };
   },
